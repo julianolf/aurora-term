a3b0509302734cd199aa5b05cd5a72add207fa8c
Juliano Fernandes <julianofernandes@gmail.com>
2019-11-20 23:47:42 -0300

    feat: add usage instructions and command line arguments parser

--- aurora_term/__main__.py
+++ aurora_term/__main__.py
@@ -1,3 +1,25 @@
-from aurora_term import terminal
+"""Amazon Aurora Serverless interactive terminal.
 
+Usage:
+    aurora-term [--profile=<p>] [--cluster=<c>] [--secret=<s>] [DATABASE]
+    aurora-term (-h | --help)
+    aurora-term --version
+
+Arguments:
+    DATABASE        Database name.
+
+Options:
+    --profile=<p>   Use a specific profile from your credential file.
+    --cluster=<c>   RDS cluster ARN.
+    --secret=<s>    RDS secret manager ARN.
+    -h --help       Show this screen.
+    --version       Show version.
+"""
+import docopt
+
+from aurora_term import __version__, config, terminal
+
+args = docopt.docopt(__doc__, version=__version__)
+prms = {k.lower().replace('--', ''): v for k, v in args.items() if v}
+conf = config.Config(**prms)
 terminal.Terminal().cmdloop()
