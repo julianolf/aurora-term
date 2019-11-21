53b69b7029cab9c76eaab4b36c804c218431520d
Juliano Fernandes <julianofernandes@gmail.com>
2019-11-21 01:18:44 -0300

    fix: terminal test by mocking boto3 lib

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -1,13 +1,16 @@
+from unittest import mock
 from aurora_term import aurora, config, terminal
 
 
-def test_create_terminal():
+@mock.patch('aurora_term.aurora.boto3')
+def test_create_terminal(m_boto3):
     term = terminal.Terminal()
     assert isinstance(term, terminal.Terminal)
     assert isinstance(term.config, config.Config)
     assert isinstance(term.aurora, aurora.Aurora)
 
 
-def test_terminal_quit_returns_true():
+@mock.patch('aurora_term.aurora.boto3')
+def test_terminal_quit_returns_true(m_boto3):
     term = terminal.Terminal()
     assert term.do_quit('')
