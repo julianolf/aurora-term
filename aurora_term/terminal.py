6106d4c3b7b0968884f64d2e9bf19cf6f9c8c5f6
Juliano Fernandes <julianofernandes@gmail.com>
2019-11-21 01:21:53 -0300

    feat: initialize aurora client (#7)
    initialize the aurora client using the given arguments passed in the command line

--- aurora_term/terminal.py
+++ aurora_term/terminal.py
@@ -1,11 +1,21 @@
 import cmd
 
-from aurora_term import __version__
+from aurora_term import __version__, aurora, config
 
 
 class Terminal(cmd.Cmd):
     intro = f'aurora-term ({__version__})\nType "help" or "?" for help.\n'
-    prompt = '=# '
+
+    def __init__(self, conf=config.Config()):
+        super().__init__()
+        self.config = conf
+        self.prompt = f'{self.config.database}=# '
+        self.aurora = aurora.Aurora(
+            profile=self.config.profile,
+            cluster_arn=self.config.cluster,
+            secret_arn=self.config.secret,
+            database=self.config.database,
+        )
 
     def do_quit(self, arg):
         """Quit aurora-term."""
